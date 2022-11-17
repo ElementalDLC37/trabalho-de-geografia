08c88fa3655d4cfd2c46624a456104ce5e29a356
Elemental <lucasantony815@gmail.com>
2022-11-17 03:38:07 -0300

    sdfvefsd

--- src/pages/Content/index.jsx
+++ src/pages/Content/index.jsx
@@ -18,42 +18,42 @@ export default function Content() {
     {
       id: 0,
       name: "Renan",
-      background: "renan.jpg",
+      background: "https://www.telegraph.co.uk/content/dam/technology/2016/12/17/JS116348132_Biko-protests-AP_trans_NvBQzQNjv4Bq0RglTN43wyGR-NQABDM7EkdijWCdRYl6PyLHMKfN-ss.jpg",
       title: "Significado",
       container: <Renan />
     },
     {
       id: 1,
       name: "Bryan",
-      background: "bryan.jpg",
+      background: "https://cdn.24.co.za/files/Cms/General/d/7072/7c4468d06b4b49e197476d39260de571.jpg",
       title: "Execução",
       container: <Bryan />
     },
     {
       id: 2,
       name: "Willian",
-      background: "willian.jpg",
+      background: "https://aidc.org.za/wp-content/uploads/2017/06/p1070908.jpg",
       title: "Tipos",
       container: <Willian />
     },
     {
       id: 3,
       name: "Bruno",
-      background: "bruno.jpg",
+      background: "https://lh3.googleusercontent.com/19abulhMBQR5D0MPyL4Q2HBDUs-LrnqUZ7TukiGFzMbvCQ8ebq9LZL6mCO05kn1spNRuf50jye8S1PYdwoCqXY9SIGr0nMwz1QEBlIoh_fvKzzg=s750",
       title: "Ações afirmativas",
       container: <Bruno />
     },
     {
       id: 4,
       name: "Abner",
-      background: "abner.jpg",
-      title: "",
+      background: "https://www.sahistory.org.za/sites/default/files/article_image/biko_funeral_saso.jpg",
+      title: "Profissões",
       container: <Abner />      
     },
     {
       id: 5,
       name: "Jordan e Gabryel",
-      background: "jordangabryel.jpg",
+      background: "https://image-prod.iol.co.za/16x9/800/Mourners-at-black-consciousness-leader-Steve-Biko-funeral-in-1977-File-photo-Independent-Archives-Original-hard-copy-stamped-26-Sep-1977?source=https://xlibris.public.prod.oc.inl.infomaker.io:8443/opencontent/objects/d9ba7b4b-c08a-5d94-97f7-9f3dca603545",
       title: "Exemplos",
       container: <JordanGabryel />
     }
@@ -88,7 +88,7 @@ export default function Content() {
   }
 
   return (
-    <div className="presenter-content" style={{ backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0.793), rgb(0, 0, 0)), url(../../../public/${content.background})`}}>
+    <div className="presenter-content" style={{ backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0.793), rgb(0, 0, 0)), url(${content.background})`}}>
       <header>
         <h3>{ content.name }</h3>
       </header>
